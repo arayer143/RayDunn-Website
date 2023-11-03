7f91bcd1a523346d6fd1683460f6cd6abbde4bac
arayer143 <89434892+arayer143@users.noreply.github.com>
2023-11-02 21:15:25 -0500

    removal of scrollbar

--- assets/vendor/js/main.js
+++ assets/vendor/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Selecao
-* Updated: Sep 18 2023 with Bootstrap v5.3.2
-* Template URL: https://bootstrapmade.com/selecao-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
