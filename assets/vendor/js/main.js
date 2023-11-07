33a2e923d818c2bc915bbeed31f6d02fe03b48b9
arayer143 <89434892+arayer143@users.noreply.github.com>
2023-11-06 20:45:48 -0600

    added a new services section

--- assets/vendor/js/main.js
+++ assets/vendor/js/main.js
@@ -244,4 +244,23 @@
     })
   });
 
+
+
+  
+
+
+  var heading = $('#our-services .desc .heading h1'),
+  txt = $('#our-services .desc .text'),
+  serviceItem = $('#our-services .services .column .service'),
+  tl = new TimelineLite();
+
+tl
+  .from(heading, 0.3, {opacity : 0, y : -20}, '+=0.3')
+  .from(txt, 0.3, {opacity : 0, y : -20})
+  .staggerFrom(serviceItem, 0.2, {x : -10, autoAlpha : 0}, 0.1);
+
+
+
+
+
 })()
