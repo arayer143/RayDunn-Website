5cd1328146f368ce450d8be861fb0017287da04e
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-05-03 16:20:17 -0500

    update to the about us section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Arsha
-* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
-* Updated: Mar 17 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 
 
 
