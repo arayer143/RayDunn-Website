90f4b36ea13ce00203ca89844580199d2c864102
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-06-05 01:00:56 -0500

    update to the contact form

--- forms/contact.php
+++ forms/contact.php
@@ -35,7 +35,7 @@
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['message'], 'Message');
 
   echo $contact->send();
 ?>
